08e557a39761d2295f671f7c0764727fb8beb88f
Ortagus Winfrey <Owatlanta@gmail.com>
2018-08-27 19:47:56 -0400

    Initial Commit

---
+++ TenHelloWorlds.java
@@ -0,0 +1,18 @@
+/*
+
+Exercise 1.1 in Java 
+
+
+
+*/
+
+public class TenHelloWorlds{
+	public static void main(String[] args){
+		for (int i=0;i<=10;i++){
+			System.out.println("Hello world");
+		}
+	
+	}
+
+
+}
